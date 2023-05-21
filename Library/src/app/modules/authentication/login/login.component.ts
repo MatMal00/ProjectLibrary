97af65d56abfc30f5ad62e2d096358e2335769c4
Patryk-Luszcz <patryk.luszcz@microsoft.wsei.edu.pl>
2023-05-21 20:53:35 +0200

    feat: handle and set authorization token into cookie service

--- Library/src/app/modules/authentication/login/login.component.ts
+++ Library/src/app/modules/authentication/login/login.component.ts
@@ -1,22 +1,29 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BooksService } from 'src/app/shared/services/books.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  providers: [CookieService],
 })
 export class LoginComponent {
   hide: boolean = true;
 
-  loginForm: FormGroup = this._formBuilder.group({
+  loginForm = this._formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
-  constructor(private _formBuilder: FormBuilder, private _booksService: BooksService, private _router: Router) {}
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _booksService: BooksService,
+    private _router: Router,
+    private _cookieService: CookieService
+  ) {}
 
   get form() {
     return this.loginForm.controls;
@@ -29,9 +36,24 @@ export class LoginComponent {
       };
 
       this._booksService.postAuthenticationLogin(login).subscribe({
-        next: (loginRes: object) => {
-          window.localStorage.setItem('user', JSON.stringify(loginRes));
+        next: (loginRes: any) => {
+          this._cookieService.set('AuthorizationToken', loginRes.accessToken);
+
+          const user = {
+            id: loginRes.id,
+            firstName: loginRes.firstName,
+            lastName: loginRes.lastName,
+            email: loginRes.email,
+            role: {
+              id: loginRes.role.id,
+              roleName: loginRes.role.roleName,
+            },
+          };
+
+          window.localStorage.setItem('user', JSON.stringify(user));
+
           this._router.navigate(['/']);
+          
           setTimeout(() => {
             document.location.reload();
           }, 1000);
