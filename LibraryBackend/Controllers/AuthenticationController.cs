c114d72bdbf17df695abbcecf6dc9f74938f8e64
matmal <mateusz.malec@microsoft.wsei.edu.pl>
2023-05-21 20:12:15 +0200

    feat: move generate token to separate method

--- LibraryBackend/Controllers/AuthenticationController.cs
+++ LibraryBackend/Controllers/AuthenticationController.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Security.Claims;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+using NuGet.Common;
 
 namespace LibraryBackend.Controllers
 {
@@ -29,34 +31,15 @@ namespace LibraryBackend.Controllers
         {
             var users = await _context.Users.ToListAsync();
 
-            var user = users.Find(user => user.Email == login.Email);   
+            var user = users.Find(user => user.Email == login.Email);
 
             if (user == null || Encoding.UTF8.GetString(user.PasswordHash) != login.Password) return BadRequest("Niepoprawny emial bądź hasło");
 
             var role = await _context.Roles.FindAsync(user.RoleId);
 
+            var tokenData = GenerateToken(user);
 
-            var claims = new[] {
-                        new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
-                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                        new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
-                        new Claim("UserId", user.Id.ToString()),
-                        new Claim("UserName", user.FirstName + " " + user.Lastname),
-                        new Claim("Email", user.Email)
-                    };
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var token = new JwtSecurityToken(
-                _configuration["Jwt:Issuer"],
-                _configuration["Jwt:Audience"],
-                claims,
-                expires: DateTime.Now.AddMinutes(20),
-                signingCredentials: signIn);
-
-            var userToken = new JwtSecurityTokenHandler().WriteToken(token);
-
-            return new LoginResponse() { Id = user.Id, Email = user.Email, FirstName = user.FirstName, Lastname = user.Lastname, Role = role, AccessToken = userToken, AccessTokenExpires = DateTime.Now.AddMinutes(10) };
+            return new LoginResponse() { Id = user.Id, Email = user.Email, FirstName = user.FirstName, Lastname = user.Lastname, Role = role, AccessToken = tokenData.UserToken, AccessTokenExpires = DateTime.Now.AddMinutes(10) };
         }
 
         [HttpPost("register")]
@@ -80,6 +63,7 @@ namespace LibraryBackend.Controllers
         }
 
         // DELETE: api/Authentication/5
+        [Authorize]
         [HttpDelete("user/{id}")]
         public async Task<IActionResult> DeleteUser(int id)
         {
@@ -93,6 +77,33 @@ namespace LibraryBackend.Controllers
             await _context.SaveChangesAsync();
 
             return NoContent();
+        }
+
+        public UserToken GenerateToken(User user)
+        {
+            var claims = new[] {
+                        new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                        new Claim("UserId", user.Id.ToString()),
+                        new Claim("UserFirstName", user.FirstName),
+                        new Claim("UserLastName", user.FirstName),
+                        new Claim("Email", user.Email),
+                        new Claim("UserRoleId", user.RoleId.ToString()),
+                    };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: signIn);
+
+            var userToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return new UserToken { ExpirationDate = DateTime.Now.AddMinutes(30), JWTToken = userToken };
         }
     }
 }
---
+++ LibraryBackend/Models/UserToken.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryBackend.Models
+{
+    public partial class UserToken
+    {
+        public string JWTToken { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+    }
+}
